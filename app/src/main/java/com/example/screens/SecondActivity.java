f3f71e8657bad0ac190ada28bb435d204c66c220
c20emili <c20emili@student.his.se>
2021-04-13 10:23:06 +0200

    Created buttons for both activitys and an textview in second activity for displaying sent text. made the buttons change activity.

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -2,7 +2,11 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        TextView secondText = findViewById(R.id.second_text);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String text = extras.getString("second_text");
+            secondText.setText(text);
+        }
+
+        Button startSecond = findViewById(R.id.stop_second);
+        startSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
